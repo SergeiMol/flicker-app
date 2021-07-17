962303dbba126ffecb4e75d6b88bf86f2a35aaa0
SergeiMol <sergei.molohvey@gmail.com>
2021-07-17 20:16:58 +0300

    Deploy project

--- src/app/components/components.module.ts
+++ src/app/components/components.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NavListComponent } from './nav-list/nav-list.component';
-import { AngularSvgIconModule } from 'angular-svg-icon';
 import { RouterModule } from '@angular/router';
 import { HeaderComponent } from './header/header.component';
 import { SearchInputComponent } from './search-input/search-input.component';
@@ -12,7 +11,6 @@ import { ReactiveFormsModule } from '@angular/forms';
   declarations: [NavListComponent, HeaderComponent, SearchInputComponent],
   imports: [
     CommonModule,
-    AngularSvgIconModule,
     RouterModule,
     ReactiveFormsModule
   ],
